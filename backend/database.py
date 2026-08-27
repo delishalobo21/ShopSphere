dc4a98794375d4b1326d9fb9f60843e922890854
Delisha Lobo <lobodelisha2@gmail.com>
2026-08-27 20:42:16 +0530

    fix database.py to use env variables

--- backend/database.py
+++ backend/database.py
@@ -1,30 +1,12 @@
-import mysql.connector
-
-
-def get_db_connection():
-
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="shopsphere"
-    )
-
-    return connection
-
-
-if __name__ == "__main__":
-
-    try:
-
-        connection = get_db_connection()
-
-        if connection.is_connected():
-            print("MySQL connection successful!")
-
-        connection.close()
-
-    except mysql.connector.Error as error:
-
-        print("MySQL connection failed:")
-        print(error)
+ import mysql.connector 
+import os 
+def get_db_connection(): 
+connection = mysql.connector.connect( host=os.environ.get("DB_HOST"),
+ user=os.environ.get("DB_USER"), 
+password=os.environ.get("DB_PASSWORD"),
+ database=os.environ.get("DB_NAME"), port=int(os.environ.get("DB_PORT", 3306)) )
+ return connection if __name__ == "__main__":
+ try: connection = get_db_connection()
+ if connection.is_connected():
+ print("MySQL connection successful!") connection.close()
+ except mysql.connector.Error as error: print("MySQL connection failed:") print(error)
